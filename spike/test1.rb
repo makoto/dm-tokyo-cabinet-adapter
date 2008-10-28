97ec87aaa4f8803265b036eef9d770d23418d4c2
Makoto Inoue <makoto@new-bamboo.co.uk>
2008-10-28 23:14:23 +0000

    Added print statment to show all entries.

--- spike/test1.rb
+++ spike/test1.rb
@@ -25,15 +25,26 @@ item.put(post1[0], Marshal.dump(post1[1]))
 item.put(post2[0], Marshal.dump(post2[1]))
 item.put(post3[0], Marshal.dump(post3[1]))
 
+p "All items."
+item.each{|e| p e}
+
 name_attr = HDB::new
 name_attr.open(data + "NameAttr.hdb", HDB::OWRITER | HDB::OCREAT)
 name_attr.put(tom[1][:name], tom[0])
 name_attr.put(peter[1][:name], peter[0])
 
+p "All name attributes"
+name_attr.each{|e| p e}
+
+
 age_attr = BDB::new
 age_attr.open(data + "AgeAttr.bdb", BDB::OWRITER | BDB::OCREAT)
 age_attr.put(tom[1][:age], tom[0])
 age_attr.put(peter[1][:age], peter[0])
+
+p "All age attributes"
+age_attr.each{|e| p e}
+
 
 p "User.first(:name => \"Tom\")"
 p Marshal.load(item.get(name_attr.get("Tom")))
@@ -55,6 +66,10 @@ title_attr.put(post1[1][:title], post1[0])
 title_attr.put(post2[1][:title], post2[0])
 title_attr.put(post3[1][:title], post3[0])
 
+p "All title attributes"
+title_attr.each{|e| p e}
+
+
 #UserIDAttr.bdb 
 # foreign_key is BDB to allow duplicate ids
 user_id_attr = BDB::new
@@ -62,6 +77,10 @@ user_id_attr.open(data + "UserIDAttr.bdb", BDB::OWRITER | BDB::OCREAT)
 user_id_attr.putlist(post1[1][:user_id], [post1[0]])
 user_id_attr.putlist(post2[1][:user_id], [post2[0]])
 user_id_attr.putlist(post3[1][:user_id], [post3[0]])
+
+p "All user id attributes"
+user_id_attr.each{|e| p e}
+
 
 p  "post1.user"
 p Marshal.load(item.get(Marshal.load(item.get(title_attr.get("Good Evening"))).user_id))
